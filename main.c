a0ff76e46e50e7f595cd857a6440a5461ff5e845
Julen Girard-grat <jgirard-@c1r6p1.42nice.fr>
2024-10-22 16:04:49 +0200

    crypting started

--- main.c
+++ main.c
@@ -7,7 +7,12 @@
 #include <elf.h>
 #include <sys/mman.h>
 
+unsigned char nazi_crypt(unsigned char byte, unsigned char prev_byte)
+{
+    byte ^= prev_byte;
+    byte += 88;
 
+}
 
 int main()
 {
@@ -22,35 +27,35 @@ int main()
 
     Elf64_Ehdr *hdr = (Elf64_Ehdr *)elf;
 
-    printf("ELF section offset: %lx\n", hdr->e_shoff);
-    printf("ELF section count: %ld\n", hdr->e_shnum);
-    printf("ELF section names index: %ld\n", hdr->e_shstrndx);
+    // printf("ELF section offset: %lx\n", hdr->e_shoff);
+    // printf("ELF section count: %ld\n", hdr->e_shnum);
+    // printf("ELF section names index: %ld\n", hdr->e_shstrndx);
 
     // e = elf, sh = section header, ent = entry, size = size, e_shentsize = elf's section header size
     Elf64_Shdr *shdr = (Elf64_Shdr *)(elf + hdr->e_shoff); /* assumes hdr->e_shentsize == (sizeof(Elf64_Shdr))  */
 
     
     uint32_t ei_class = *(uint32_t *)(elf + EI_CLASS);
-    if (elf[EI_MAG0] != ELFMAG0  || elf[EI_MAG1] != ELFMAG1 ||
-        elf[EI_MAG2] != ELFMAG2 || elf[EI_MAG3] != ELFMAG3)
-        {
-            printf("The file isn't a ELF compliant file\n");
-            return;
-        }
+    // if (elf[EI_MAG0] != ELFMAG0  || elf[EI_MAG1] != ELFMAG1 ||
+    //     elf[EI_MAG2] != ELFMAG2 || elf[EI_MAG3] != ELFMAG3)
+    //     {
+    //         printf("The file isn't a ELF compliant file\n");
+    //         return;
+    //     }
 
-        switch(elf[4]) {
-        case ELFCLASSNONE:
-            printf("Invalid class\n");
-            break;
-        case ELFCLASS32:
-            printf("32-bit objects\n");
-            break;
-        case ELFCLASS64:
-            printf("64-bit objects\n");
-            break;
-        default:
-            printf("Unknown class\n");
-    }
+    //     switch(elf[4]) {
+    //     case ELFCLASSNONE:
+    //         printf("Invalid class\n");
+    //         break;
+    //     case ELFCLASS32:
+    //         printf("32-bit objects\n");
+    //         break;
+    //     case ELFCLASS64:
+    //         printf("64-bit objects\n");
+    //         break;
+    //     default:
+    //         printf("Unknown class\n");
+    // }
     // elf 0x0 - ...
     // ...
     // liste des noms - 0x1000
@@ -71,11 +76,26 @@ int main()
     {
         if (strcmp(sec_names + shdr[i].sh_name, ".text") == 0)
         {
+            shdr[i].sh_flags |= SHF_WRITE;
+            char *ptr = elf + shdr[i].sh_offset;
             const char *txt_sectn = (const char *)sec_names + shdr[i].sh_name;
+            char prev_byte = 0;
+            for (int y = 0; y < shdr[i].sh_size; y++)
+            {
+                char tmp = ptr[y];
+                ptr[y] = crypt(ptr[y], prev_byte);
+                prev_byte = tmp;
+            }
+            
             break;
         }
     }
 
+    FILE *outputfp = fopen("couilles", "wb");
+    
+    fwrite(elf, 1, size, outputfp);
+    fclose(outputfp);
+    return;
     // void *mapped_file = mmap(NULL, hdr->e_phnum * sizeof(Elf64_Phdr), PROT_READ | PROT_WRITE, MAP_PRIVATE, fileno(fp), 0);
     // if (mapped_file == MAP_FAILED)
     // {
@@ -87,21 +107,26 @@ int main()
     Elf64_Phdr *phdr = elf + hdr->e_phoff;
     //Elf64_Shdr *text_section = &shdr[(int)text_section - hdr->e_shstrndx]; 
     //off_t txtoff = text_section->sh_offset;
-    for (int i = 0; i < hdr->e_phnum; i++)
-    {
-        if(phdr[i].p_type == PT_LOAD && (phdr[i].p_flags & PF_X)) {
-            char *txt_start = elf + phdr[i].p_offset;
-            //size_t txt_size = phdr[i].p_filesz;
+    // for (int i = 0; i < hdr->e_phnum; i++)
+    // {
+    //     if(phdr[i].p_type == PT_LOAD && (phdr[i].p_flags & PF_X)) {
+    //         char *txt_start = elf + phdr[i].p_offset;
+    //         //size_t txt_size = phdr[i].p_filesz;
 
-            printf("Contents of .text section:\n");
-            fwrite(txt_start, 1, 10, stdout);
-            // munmap(mapped_file,hdr->e_phnum * sizeof(Elf64_Phdr));
-            //fclose(fp);
-            //return;
-        }
-    }
-     
-    // for (int i = 0; i < hdr->e_shnum; i++) {
-    //     printf("name: %s offset: %lx\n" sec_names + shdr[i].sh_name, shdr[i].sh_offset);
+    //         printf("Contents of .text section:\n");
+    //         fwrite(txt_start, 1, 10, stdout);
+    //         // munmap(mapped_file,hdr->e_phnum * sizeof(Elf64_Phdr));
+    //         //fclose(fp);
+    //         //return;
+    //     }
     // }
+     
+    for (int i = 0; i < hdr->e_shnum; i++) {
+        printf("name: %s offset: %lx banderas: %lx, res: %lx\n", sec_names + shdr[i].sh_name, shdr[i].sh_offset, shdr[i].sh_flags, shdr[i].sh_flags, (SHF_ALLOC | SHF_EXECINSTR));
+        if ((shdr[i].sh_flags & (SHF_ALLOC | SHF_EXECINSTR)) == (SHF_ALLOC | SHF_EXECINSTR))
+        {
+            printf("wow jb found :00000 \n");
+        }
+        
+    }
 }
